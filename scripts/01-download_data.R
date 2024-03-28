4d93ad7dcd9d38f89a7874fd4809d977c8930f41
Catherine Punnoose <catherinee1216@gmail.com>
2024-03-28 10:29:57 -0400

    more updates

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,5 +1,5 @@
 #### Preamble ####
-# Purpose: Replicated graphs from... [...UPDATE THIS...]
+# Purpose: Downloads and saves the data from [...UPDATE THIS...]
 # Author: Rohan Alexander [...UPDATE THIS...]
 # Date: 11 February 2023 [...UPDATE THIS...]
 # Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
@@ -9,11 +9,18 @@
 
 
 #### Workspace setup ####
+library(opendatatoronto)
 library(tidyverse)
 # [...UPDATE THIS...]
 
-#### Load data ####
-# [...ADD CODE HERE...]
+#### Download data ####
+# [...ADD CODE HERE TO DOWNLOAD...]
 
 
 
+#### Save data ####
+# [...UPDATE THIS...]
+# change the_raw_data to whatever name you assigned when you downloaded it.
+write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+
+         
